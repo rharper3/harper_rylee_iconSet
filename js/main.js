737b622624211ef8f51611ef3da0faad61cad863
r_harper3 <r_harper3@fanshaweonline.ca>
2019-01-26 14:17:15 -0500

    started base html

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(() =>{
+
+	// self-invoking anon function (or Lambda)
+	// start of every js file (() =>{          })()
+	// var => means create a variable
+	// whatever comes after the = is what the variable ref
+	
+
+	var images = document.querySelectorAll('img');
+
+	//document is always the webpage
+	// after the dot is always a method or a property
+
+	console.log(images);
+
+})()
